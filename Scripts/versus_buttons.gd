b05a2a2d53573759dfd9dc95332bc505d33db2f5
Bella Lu <bellalu@Bellas-MacBook-Pro.local>
2026-07-09 18:36:33 -0700

    timer again

--- Scripts/versus_buttons.gd
+++ Scripts/versus_buttons.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var timer_label: Label = $TimerLabel
 
+var time_left := 0.0
+
 @export var label_text := "Default Text":
 	set(value):
 		label_text = value
@@ -11,20 +13,27 @@ extends Node2D
 
 func _ready():
 	$LevelName.text = label_text
-
-
-func _on_rotation_toggled(toggled_on: bool) -> void:
-	Multiplayer.rotation_mode = toggled_on
+	# Start countdown immediately from selected stage time
+	time_left = Multiplayer.stage_one_time
+	await get_tree().create_timer(3.0).timeout
+	$Instructions.visible = false
 
 
 func _process(delta):
-	if Multiplayer.stage_one_time > 0:
-		Multiplayer.stage_one_time -= delta
+	if time_left > 0:
+		time_left -= delta
 	
 	update_timer_display()
 
+
 func update_timer_display():
-	var minutes = int(Multiplayer.stage_one_time) / 60
-	var seconds = int(Multiplayer.stage_one_time) % 60
+	var minutes = int(time_left) / 60
+	var seconds = int(time_left) % 60
 	
 	timer_label.text = "%02d:%02d" % [minutes, seconds]
+
+	# Last 30 seconds turns red
+	if time_left <= 30:
+		timer_label.modulate = Color.RED
+	else:
+		timer_label.modulate = Color.WHITE
