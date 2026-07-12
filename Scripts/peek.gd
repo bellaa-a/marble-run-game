a3d25637bd13988d4cb7ae5ddf6d54023e3f8785
Bella Lu <bellalu@Bellas-MacBook-Pro.local>
2026-07-12 14:10:43 -0700

    typo

--- Scripts/peek.gd
+++ Scripts/peek.gd
@@ -1,12 +1,11 @@
 extends Control
 
-
+@onready var eyes = $Eyes
 @onready var player1 = $Eyes/Player1
 @onready var player2 = $Eyes/Player2
 
 func _ready() -> void:
-	player1.global_position = Multiplayer.pipe_position + Vector2(10, 0)
-	player2.global_position = Multiplayer.pipe_position + Vector2(10, 0)
+	eyes.global_position = Multiplayer.pipe_position + Vector2(10, 0)
 	$Username.text = Multiplayer.get_opponent_name()
 	if Multiplayer.opponent_is_host():
 		player1.visible = true
